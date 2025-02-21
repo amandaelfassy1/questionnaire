ad100197b91b92688e46ed838b6d30c7b7c3eec7
Amanda Elfassy <amandawebstart@gmail.com>
2025-02-21 15:00:08 +0100

    style register login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">       
-        @if (app()->environment('local'))
-        @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- 🔥 Utilise Vite en local -->
-            @else
-                <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-                <script src="{{ asset('build/assets/app.js') }}" defer></script>
-            @endif
-        </head>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen  dark:bg-white-900">
             <!-- Page Heading -->
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: process.env.APP_ENV === 'local' ? { // 🔥 Condition pour local
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws', // ✅ WebSocket fonctionne bien en local (HTTP)
+        }
+    } : {
+        https: true, // ✅ Active HTTPS en production
+        hmr: {
+            protocol: 'wss' // ✅ WebSocket sécurisé pour Heroku
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        hmr: {
-            protocol: 'wss' 
-        }
-    },
 });
