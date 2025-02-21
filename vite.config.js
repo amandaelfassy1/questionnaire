748ae233935b3bc844d051c5e49ca71f9b18a1a9
Amanda Elfassy <amandawebstart@gmail.com>
2025-02-21 14:51:18 +0100

    style register login

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: 'localhost',
-            protocol: 'ws', // 🔥 Utilise WebSocket en HTTP pour éviter les erreurs en local
-        }
-    },
-    build: {
-        manifest: true, // 🔥 Génère un manifest pour que Laravel charge bien les assets en prod
-        outDir: 'public/build', // 🔥 Stocke les fichiers compilés dans public/build pour Heroku
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+   
 });
