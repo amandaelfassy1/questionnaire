d40af5bbe53f6e5000ca05c9386c55f0c247bb7b
Amanda Elfassy <amandawebstart@gmail.com>
2025-02-22 22:27:00 +0100

    add style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,16 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        @if (app()->environment('local'))
-        @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- ✅ Vite utilisé en local -->
+        @if (app()->environment('production'))
+        @php
+            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        @endphp
+        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+        <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
     @else
-        <link rel="stylesheet" href="{{ secure_asset('build/assets/app.css') }}">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
+    
     
     </head>
     <body class="font-sans antialiased">
