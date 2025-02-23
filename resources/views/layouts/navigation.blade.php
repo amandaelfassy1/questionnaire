4ca191df34492698ff1b0ba895c1a13727754c09
Amanda Elfassy <amandawebstart@gmail.com>
2025-02-23 19:37:14 +0100

    add style

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white  dark:bg-gray-800  dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ url('/') }}">
-                        <img src="{{asset('img/e.png')}}" style="width: 160px"/>
+                        <img src="{{ asset('img/e.png') }}" style="width: 160px"/>
                     </a>
                 </div>
                 <!-- Navigation Links -->
@@ -16,20 +16,19 @@
                     </x-nav-link>
                 </div>
                 @if(Auth::check() && Auth::user()->email === 'test@gmail.com')
-
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link class="text-primary" href="{{ route('admin.dashboard') }}">Admin</x-nav-link>
                 </div>
-        @endif
+                @endif
             </div>
 
             <!-- Settings Dropdown -->
+            @if(Auth::check()) <!-- Si l'utilisateur est connecté -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -46,17 +45,23 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
             </div>
-
+            @else <!-- Si l'utilisateur n'est pas connecté -->
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <x-nav-link href="{{ route('login') }}">
+                    {{ __('Connexion') }}
+                </x-nav-link>
+                <x-nav-link href="{{ route('register') }}">
+                    {{ __('Inscription') }}
+                </x-nav-link>
+            </div>
+            @endif
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
@@ -71,31 +76,36 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        
-
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+            @if(Auth::check())
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-            
-        
+            @else
+            <div class="px-4">
+                <x-responsive-nav-link href="{{ route('login') }}">
+                    {{ __('Connexion') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link href="{{ route('register') }}">
+                    {{ __('Inscription') }}
+                </x-responsive-nav-link>
+            </div>
+            @endif
+
             <div class="mt-3 space-y-1">
+                @if(Auth::check())
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
-
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
+                @endif
             </div>
         </div>
     </div>
